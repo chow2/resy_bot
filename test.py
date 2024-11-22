41939552dcf3589c2e12cc9d7c89473a0ea245fb
chowleyTCE <chowley@tcelect.net>
2024-11-22 13:44:21 -0500

    added logins to xl, testing file

---
+++ test.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+EMAIL = os.getenv('EMAIL')
+PASSWORD = os.getenv('PASSWORD')
+RESTAURANT_NAME = os.getenv('RESTAURANT_NAME')
+DATE = os.getenv('DATE')
+TARGET_TIME = os.getenv('TARGET_TIME')
+EARLIEST = os.getenv('EARLIEST')
+LATEST = os.getenv('LATEST')
+PARTY_SIZE = os.getenv('PARTY_SIZE')
+
+if __name__ == "__main__":
+
+
+    print(EMAIL) 
+    print(PASSWORD) 
+    print(RESTAURANT_NAME)
+    print(DATE)
+    print(TARGET_TIME)
+    print(EARLIEST)
+    print(LATEST)
+    print(PARTY_SIZE)
+
+    
